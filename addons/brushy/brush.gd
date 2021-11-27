3e1be377b3732cbcbd39bd7995c75804f1230ec8
Josh Jones <kilauea.jones@gmail.com>
2021-11-27 15:26:56 -0800

    Optimized generation of visual mesh by combining surfaces when possible
    Surfaces on a brush that share the same material will now be combined, reducing the number of draw calls required.  This has the bonus benefit of improving generation time as well.
    
    Also made it possible to turn off generation of the visual mesh entirely, for things like Areas that would only use the collision shape.

--- addons/brushy/brush.gd
+++ addons/brushy/brush.gd
@@ -3,29 +3,36 @@ class_name Brush
 extends Spatial
 
 
-var mesh_instance := MeshInstance.new();
-
-var collision_parent: CollisionObject;
-var collision_owner_id := -1;
 var collision_enabled := true setget _set_collision_enabled;
 var collision_shape: Shape setget _set_collision_shape;
+var collision_parent: CollisionObject;
+var collision_owner_id := -1;
 
-var default_material = SpatialMaterial.new();
+var visual_enabled := true setget _set_visual_enabled;
+var visual_mesh: Mesh setget _set_visual_mesh;
+var visual_mesh_instance := MeshInstance.new();
+
+var faces_dirty := false;
 var faces := [];
 
 
 func _init() -> void:
 	# TODO: Debug logic, remove when generating shapes via tool.
-	var planes := Geometry.build_cylinder_planes(1, 2, 16, Vector3.AXIS_Y);
+	#var planes := Geometry.build_cylinder_planes(1, 2, 12, Vector3.AXIS_Y);
+	#var planes := Geometry.build_capsule_planes(1, 2, 6, 3, Vector3.AXIS_Y);
+	var planes := Geometry.build_box_planes(Vector3(1, 1, 1));
+	var default_material = SpatialMaterial.new();
+	
 	for plane in planes:
 		faces.append(BrushFace.new(plane, default_material, Transform2D.IDENTITY));
 	
-	add_child(mesh_instance);
+	add_child(visual_mesh_instance);
+	set_notify_transform(true);
 
 
 func _ready() -> void:
-	set_notify_transform(true);
-	_update_meshes();
+	# TODO: Remove this when we can save/load face data from scene.  Changing face data should mark dirty.
+	mark_faces_dirty();
 
 
 func _get_property_list() -> Array:
@@ -44,6 +51,30 @@ func _get_property_list() -> Array:
 		{
 			"name": "collision_enabled",
 			"type": TYPE_BOOL
+		},
+		{
+			"name": "collision_shape",
+			"type": TYPE_OBJECT,
+			"usage": PROPERTY_USAGE_EDITOR,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Shape"
+		},
+		{
+			"name": "Visual",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_GROUP,
+			"hint_string": "visual_"
+		},
+		{
+			"name": "visual_enabled",
+			"type": TYPE_BOOL
+		},
+		{
+			"name": "visual_mesh",
+			"type": TYPE_OBJECT,
+			"usage": PROPERTY_USAGE_EDITOR,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Mesh"
 		}
 	];
 
@@ -82,8 +113,10 @@ func _notification(what: int) -> void:
 
 
 func _set_collision_enabled(value: bool) -> void:
+	if (collision_enabled == value):
+		return;
+	
 	collision_enabled = value;
-	update_gizmo();
 	
 	if (collision_parent):
 		collision_parent.shape_owner_set_disabled(collision_owner_id, !collision_enabled);
@@ -95,7 +128,6 @@ func _set_collision_shape(value: Shape) -> void:
 		return;
 	
 	collision_shape = value;
-	update_gizmo();
 	
 	if (collision_parent):
 		collision_parent.shape_owner_clear_shapes(collision_owner_id);
@@ -104,45 +136,105 @@ func _set_collision_shape(value: Shape) -> void:
 		collision_parent.update_gizmo();
 
 
-func _update_meshes() -> void:
-	for face in faces:
-		face.build_surface_data(faces);
+func _set_visual_enabled(value: bool) -> void:
+	if (visual_enabled == value):
+		return;
 	
-	mesh_instance.mesh = build_visual_mesh();
+	visual_enabled = value;
+	
+	if (visual_enabled):
+		self.visual_mesh = build_visual_mesh();
+	else:
+		self.visual_mesh = null;
+
+
+func _set_visual_mesh(value: Mesh) -> void:
+	if (visual_mesh == value):
+		return;
+	
+	visual_mesh = value;
+	visual_mesh_instance.mesh = value;
+	property_list_changed_notify();
+
+
+func mark_faces_dirty() -> void:
+	if (faces_dirty):
+		return;
+	
+	faces_dirty = true;
+	call_deferred("_update_meshes");
+
+
+func _update_meshes() -> void:
+	_update_face_data();
+	
+	if (visual_enabled):
+		self.visual_mesh = build_visual_mesh();
+	else:
+		self.visual_mesh = null;
+	
 	self.collision_shape = build_collision_shape();
 
 
+func _update_face_data() -> void:
+	var start_time := OS.get_ticks_usec();
+	
+	for face in faces:
+		face.build_surface_data(faces);
+	
+	print_debug("Face data gen time: ", (OS.get_ticks_usec() - start_time) / 1000.0, "ms");
+
+
 func build_visual_mesh() -> Mesh:
+	var start_time := OS.get_ticks_usec();
+	
 	var surface_tool := SurfaceTool.new();
 	var visual_mesh := ArrayMesh.new();
 	
+	# Group faces by their material
+	var faces_by_material := Dictionary();
 	for i in range(faces.size()):
 		var face: BrushFace = faces[i];
-		
+		if (faces_by_material.has(face.material)):
+			faces_by_material[face.material].append(face);
+		else:
+			faces_by_material[face.material] = [ face ];
+	
+	# Create a surface per material
+	for material in faces_by_material:
 		surface_tool.clear();
 		surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES);
-		surface_tool.set_material(face.material);
+		surface_tool.set_material(material);
 		
-		var vertex_data := PoolVector3Array();
-		var uv_data := PoolVector2Array();
-		var color_data := PoolColorArray();
-		var uv2_data := PoolVector2Array();
-		var normal_data := PoolVector3Array();
-		var tangent_data := Array();
+		var faces = faces_by_material[material];
+		for i in range(faces.size()):
+			var face: BrushFace = faces[i];
+			
+			var vertex_data := PoolVector3Array();
+			var uv_data := PoolVector2Array();
+			var color_data := PoolColorArray();
+			var uv2_data := PoolVector2Array();
+			var normal_data := PoolVector3Array();
+			var tangent_data := Array();
+			
+			for vertex in face.vertices:
+				# TODO: Compute UVs
+				vertex_data.append(vertex);
+				normal_data.append(face.plane.normal);
+				tangent_data.append(face.plane);
+			
+			surface_tool.add_triangle_fan(vertex_data, uv_data, color_data, uv2_data, normal_data, tangent_data);
 		
-		for vertex in face.vertices:
-			# TODO: Compute UVs
-			vertex_data.append(vertex);
-			normal_data.append(face.plane.normal);
-			tangent_data.append(face.plane);
-		
-		surface_tool.add_triangle_fan(vertex_data, uv_data, color_data, uv2_data, normal_data, tangent_data);
 		surface_tool.commit(visual_mesh);
+	
+	print_debug("Visual Mesh gen time: ", (OS.get_ticks_usec() - start_time) / 1000.0, "ms");
 	
 	return visual_mesh;
 
 
 func build_collision_shape() -> Shape:
+	var start_time := OS.get_ticks_usec();
+	
 	var collision_vertices := PoolVector3Array();
 	for i in range(faces.size()):
 		var face: BrushFace = faces[i];
@@ -160,6 +252,9 @@ func build_collision_shape() -> Shape:
 	
 	var shape := ConvexPolygonShape.new();
 	shape.points = collision_vertices;
+	
+	print_debug("Collision Shape gen time: ", (OS.get_ticks_usec() - start_time) / 1000.0, "ms");
+	
 	return shape;
 
 
